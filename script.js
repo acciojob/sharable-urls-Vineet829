e9a9a055b455f89e966d2373c61e85f4e597a3bc
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-13 16:54:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-let change = document.querySelector("#urlForm").addEventListener("submit", (event) => {
-    event.preventDefault();
-    let name = document.querySelector("#name").value.trim();
-    let year = document.querySelector("#year").value;
-    let queryString = "https://localhost:8080/";
-    
-    if (name) {
-        queryString += `?name=${encodeURIComponent(name)}`;
-    }
-    
-    if (year) {
-        queryString += (name ? "&" : "?") + `year=${encodeURIComponent(year)}`;
-    }
-    
-    document.querySelector("#url").innerText = queryString;
+document.addEventListener("DOMContentLoaded", () => {
+    let change = document.querySelector("#urlForm").addEventListener("submit", (event) => {
+        event.preventDefault();
+        let name = document.querySelector("#name").value.trim();
+        let year = document.querySelector("#year").value;
+        let queryString = "https://localhost:8080/";
+        
+        if (name) {
+            queryString += `?name=${encodeURIComponent(name)}`;
+        }
+        
+        if (year) {
+            queryString += name ? "&" : "?" + `year=${encodeURIComponent(year)}`;
+        }
+        
+        document.querySelector("#url").innerText = queryString;
+    });
 });
-
